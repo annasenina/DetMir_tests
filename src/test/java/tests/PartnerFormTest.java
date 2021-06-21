487876c7ab58575c18955f203836522bd1a6a256
Anna Senina <as.senina@gmail.com>
2021-06-21 16:48:42 +0300

    Добавляем настройки драйвера для удаленного запуска

--- src/test/java/tests/PartnerFormTest.java
+++ src/test/java/tests/PartnerFormTest.java
@@ -1,16 +1,22 @@
 package tests;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Tag;
 
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
+import static io.qameta.allure.Allure.step;
 
-public class PartnerFormTest {
+public class PartnerFormTest extends TestBase{
 
     @Test
     @Tag("web")
+    @DisplayName("Проверяем наличие формы \"Информация о поставщике\"")
     public void CanOpenPartnerPageTest(){
-        open("https://partner.detmir.market/form");
+        step ("Открываем страницу для партнеров", ()-> {
+            open("");
+        });
+
     }
 }
